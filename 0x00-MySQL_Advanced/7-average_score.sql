8a6774ea7cf1bfce40a770e36c9c3d351187bbb1
mumbuamutuku <mumbuamutuku@gmail.com>
2023-07-13 08:35:15 +0000

    7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,3 +1,4 @@
+--  SQL script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student.
 DELIMITER //
 
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
